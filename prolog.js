6cbc87c4d8d02bcf87b5bfac571010735c257ef9
jldupont <github@jldupont.com>
2015-10-22 00:38:08 +0000

    small fix

--- prolog.js
+++ prolog.js
@@ -119,8 +119,9 @@ Prolog.compile_per_sentence = function(parsed_sentences) {
                 code_object = c.process_query( parsed_sentence.args[0] );
             } else {
                 code_object = c.process_rule_or_fact(parsed_sentence);
-                result.push( new Code(code_object) );
             }
+            
+            result.push( new Code(code_object) );
             
         } catch(e) {
             result.push(e);
