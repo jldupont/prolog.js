1bae5592b33800f475e91ede0bf02a0010098a9a
jldupont <github@jldupont.com>
2015-10-24 13:05:06 +0000

    clean up

--- dist/bower_components/require/examples/loading/js/lib/others/sub.js
+++
@@ -1,3 +0,0 @@
-module.exports = function sub(a, b) {
-    return a - b;
-};
--- dist/bower_components/require/examples/loading/route/index.js
+++
@@ -1,7 +0,0 @@
-function Route(name) {
-
-    this.name = name;
-    this.path = require.resolve("../js/");
-}
-
-module.exports = Route;
